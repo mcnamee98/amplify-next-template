ed56fe8dab7e259cb08b2674fcd01c2e2c96f59d
mcnamee98 <jmcnamee@andrew.cmu.edu>
2026-01-23 14:18:23 -0500

    added authenitcator
    Login + sign up capabilities now in place

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,14 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-
+import { useAuthenticator } from "@aws-amplify/ui-react";
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -53,6 +54,8 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
